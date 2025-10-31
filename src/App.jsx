6bee9124a6750b706546813474c37d5a47b1e8de
Fabio Silveira <fabio.eds@hotmail.com>
2025-10-30 18:47:53 -0700

    AddedCode

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,38 @@
-import axios from "axios"
+import axios from "axios";
 import { useEffect, useState } from "react"
 
 export default function App() {
-
-  const [data, setData] = useState([]);
+  
+  const [players, setPlayers] = useState([]);
   const [searchPlayer, setSearchPlayer] = useState("");
 
   useEffect(() => {
-    try {
-      async function fetchApi() {
-        const req = await axios.get('https://api.sleeper.app/v1/players/nfl')
-        const reqArray = Object.values(req.data)
-        const fifteenPlayers = reqArray.slice(0, 15)
-        setData(fifteenPlayers)
-      }
-      fetchApi();
-    } catch (error) {
-      console.error("we could not fetch the data")
+    async function fetchApi() {
+      const req = await axios.get('https://api.sleeper.app/v1/players/nfl')
+      const reqArray = Object.values(req.data)
+      const fifteenPlayers = reqArray.slice(0, 15)
+      setPlayers(fifteenPlayers)
     }
+    fetchApi();
   }, [])
 
-  const filteredPlayers = data.filter((e) => `${e.full_name}`.toLowerCase().includes(searchPlayer.toLowerCase()))
-
+  const filteredPlayers = players.filter((e) => `${e.full_name}`.toLowerCase().includes(searchPlayer.toLowerCase()))
+  
   return (
     <>
-
-    <input
-     placeholder="enter a player"
-     type="text"
-     value={searchPlayer}
-     onChange={(event) => setSearchPlayer(event.target.value)}
+    <input 
+      placeholder="enter a player"
+      type="text"
+      value={searchPlayer}
+      onChange={(event) => setSearchPlayer(event.target.value)}
     />
-     
       <table>
         <thead>
           <tr>
-            <th>Name</th>
+            <th>Player</th>
             <th>Age</th>
             <th>Position</th>
+            <th>College</th>
           </tr>
         </thead>
         <tbody>
@@ -46,6 +41,7 @@ export default function App() {
               <td>{e.full_name}</td>
               <td>{e.age}</td>
               <td>{e.position}</td>
+              <td>{e.college}</td>
             </tr>
           ))}
         </tbody>
